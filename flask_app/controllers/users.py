0c3b1c283340ae60082aa92ba946966cf93f8c6d
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-03-01 10:46:33 -0800

    commit

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, session, flash
 from flask_app import app
 from flask_app.models.user import User
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app) 
 
 @app.route('/', methods=['GET', 'POST'])
 def login_and_register():
@@ -11,25 +13,26 @@ def login_and_register():
             'first_name': request.form['first_name'],
             'last_name': request.form['last_name'],
             'email': request.form['email'],
-            'password': request.form['password']
+            'password': bcrypt.generate_password_hash(request.form['password'])
         }
         user = User(**data)
         user.register()
-        return render_template('profile.html')
+        return render_template('profile.html', user=user)
     else:
         return render_template('login_and_registration.html')
     
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form['login_email']
-    password = request.form['login_password']
     user = User.get_by_email(email)
-    if user and user.password == password:
-        session['user_id'] = user.id
-        return render_template('profile.html', user=user)
-    else:
-        flash('Invalid email or password')
+    if not user:
+        flash("Invalid Email/Password")
         return redirect('/')
+    if not bcrypt.check_password_hash(user.password, request.form['login_password']):
+        flash("Invalid Email/Password")
+        return redirect('/')
+    session['user_id'] = user.id
+    return render_template('profile.html', user=user)
 
 @app.route('/logout')
 def logout():
